5a305044417cb84883131247ebf07d531f6dd3f7
bmerlet <benjamin.merlet@toulouse.inra.fr>
2016-10-10 16:39:00 +0200

    Update of ant build and added app

---
+++ src/parsebionet/enrichment/EnrichMetaboliteIds.java
@@ -0,0 +1,22 @@
+package parsebionet.enrichment;
+
+import org.kohsuke.args4j.Option;
+
+public class EnrichMetaboliteIds {
+
+	
+	@Option(name="-methost", usage="distinct metexplore host you are working on")
+	public String metHost = "";
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("testing build");
+	}
+
+}
